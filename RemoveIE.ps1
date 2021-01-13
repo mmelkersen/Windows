4180c560350b6c27ed42f59220c1cda09d538768
mmelkersen <77377683+mmelkersen@users.noreply.github.com>
2021-01-13 08:36:18 +0100

    Delete RemoveIE.ps1

--- RemoveIE.ps1
+++
@@ -1,8 +0,0 @@
-﻿#Powershell Script To remove Interner Explorer
-#Check If remove Interner Explorer Are Already Installed
-$check = Get-WindowsOptionalFeature -Online | Where-Object {$_.FeatureName -eq "Internet-Explorer-Optional-amd64"}
-If ($check.State -ne "Disabled")
-{
-#Remove Internet Explorer
-Disable-WindowsOptionalFeature -FeatureName Internet-Explorer-Optional-amd64 -Online -NoRestart | Out-Null
-}
