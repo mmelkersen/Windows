862d8141e3b47d7fab76d728c6c37c91051132e2
MMelkersen <matttiasmelkersen@outlook.com>
2021-03-19 21:22:23 +0100

    ActivateWindowsScript

---
+++ License/Activate_Windows.ps1
@@ -0,0 +1,29 @@
+﻿<#
+======================================================================================================
+ 
+ Created on:    12.03.2021
+ Created by:    Mattias Melkersen
+ Version:       0.1  
+ Mail:          mm@mindcore.dk
+ twitter:       @mmelkersen
+ Function:      Activating Windows from the built-in BIOS key. Can be used to downgrade OS
+
+======================================================================================================
+
+#>
+Try
+{
+
+        #Activating device from BIOS Serial
+        $key = (get-wmiobject -class softwarelicensingservice).OA3xOriginalProductKey
+        Cscript.exe C:\Windows\System32\slmgr.vbs -ipk $key
+        
+        exit 0        
+}
+
+catch
+{
+    $errMsg = $_.Exception.Message
+    return $errMsg
+    exit 1
+}
